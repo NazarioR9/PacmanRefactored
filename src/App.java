d9b3dccd1f93f022b88dfe9ef24e815884810382
nazario <NazarioR9@github.com>
2020-01-03 21:15:52 +0100

    move 'App' to the project base

---
+++ src/App.java
@@ -0,0 +1,59 @@
+
+
+import javax.swing.JOptionPane;
+
+import gui.Gui;
+import model.Game;
+import utilities.Utils;
+
+public class App {
+	Game game;
+	Gui gui;
+	
+	public App() {
+		game = new Game();
+		gui = new Gui(game);
+	}
+	
+	public void run() {
+		while(!game.noMoreGome() && !game.noMoreLife()) {
+			game.play();
+			gui.paint();
+			Utils.sleep(150);
+		}
+		
+		String msg;
+		if(game.noMoreLife()) msg = "Game Over";
+		else msg = "You've won";
+		
+		ask2Play(msg);
+	}
+	
+	public void reset() {
+		game.totalReset();
+	}
+	
+	public void exit() {
+		gui.quit();
+	}
+	
+	public void ask2Play(String msg) {
+		String response = JOptionPane.showInputDialog(null, msg+"\nPlay again ?\n y: yes  n: no");
+		System.out.println("Response : " + response);
+		
+		if(response.equals("y")) {
+			reset();
+			run();
+		}else if(response.equals("n")) {
+			exit();
+		}else {
+			ask2Play("Didn't get ypur response.");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		App app = new App();
+		app.run();
+	}
+}
